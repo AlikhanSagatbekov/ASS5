35066a8f2ea86310797c99e1938415b22cf180a9
AlikhanSagatbekov <221112@astana.edu.kz>
2023-05-21 23:41:57 +0600

    implement test in main class

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        BST<Integer, String> b3 = new BST<>();
+        b3.put(2, "asd");
+        b3.put(1, "qwe");
+        b3.put(4, "qwer");
+        b3.put(3, "ierufieug");
+        b3.put(5, "jalil");
+
+        for (Integer key : b3) {
+            String value = b3.get(key);
+            System.out.println("key is " + key + " and value is " + value);
+        }
+
     }
 }
